257b83b6492eb7bb6fa671406d6562bc38a0ffb9
Linda Jensen <linda.jensen81@me.com>
2023-09-12 21:31:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let add = document.querySelector("#add")
-let subtract = document.querySelector("#subtract")
+let add = document.querySelector("#add");
+let subtract = document.querySelector("#subtract");
 
 console.log(add, subtract);
+
+add.addEventListener("click", function () {
+    let output = document.querySelector("output");
+    let result = Number(output.innerText) + 1;  
+
+    output.innerText = result;
+  
+});
